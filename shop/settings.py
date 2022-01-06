a1be422f2c8976bc1236d7f1b164cfc1ee4cea39
xtimule <gxumoyun1@gmail.com>
2022-01-06 19:09:15 +0500

    changes

--- shop/settings.py
+++ shop/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, '../product/static')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'product/static')
-]
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
