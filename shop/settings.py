0d2d278e01a4e19f7216cdcc509ba9c9b5f0c0df
xtimule <gxumoyun1@gmail.com>
2022-01-06 19:15:27 +0500

    changes

--- shop/settings.py
+++ shop/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
